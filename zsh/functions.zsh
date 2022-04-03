30b51a903a1d73613d9943dfea308bb7585d2251
Oliver Vogel <oliver@olivervogel.com>
2022-04-03 11:18:32 +0200

    Added ctags zsh function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -154,3 +154,6 @@ _slugify () {
     echo "$1" | iconv -c -t ascii//TRANSLIT | sed -E 's/[~^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z
 }
 
+_ctags_create_php () {
+    ctags --tag-relative=yes -R --fields=+aimlS --languages=php --PHP-kinds=+cdfint-av --exclude="\.git" --exclude="node_modules"
+}
