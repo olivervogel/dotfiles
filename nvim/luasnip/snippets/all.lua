a0de207d4a20d45ab8341452efbf095fdf3ee456
Oliver Vogel <oliver@olivervogel.com>
2023-01-24 17:10:40 +0100

    (vim) Migrate snippets to LuaSnip

--- nvim/luasnip/snippets/all.lua
+++ nvim/luasnip/snippets/all.lua
@@ -1,11 +1,25 @@
 return {
 
-  s("fox", { 
-  	t("The quick brown fox jumps over the lazy dog")
+	s("fox", { 
+		t("The quick brown fox jumps over the lazy dog")
 	}),
 
-  s("lorem", { 
-  	t("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+	s("lorem", { 
+		t("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 	}),
+
+	s("tt", fmt([[
+	function {name}({args}) {{
+		{body}
+	}}
+	]], {
+		name = i(1, "myFunction"),
+		args = c(2, { t("args"), t("") }, { node_ext_opts = {
+			passive = {
+				virt_text = {{"args | nil"}}
+			}
+		}}),
+		body = c(3, { t("//"), i(1, "value") }),
+	})),
 
 }
--- nvim/luasnip/snippets/html.lua
+++ nvim/luasnip/snippets/html.lua
@@ -1,9 +1,33 @@
 return {
 
-  s("css", fmt([[
-  <link rel="stylesheet" type="text/css" href="{}">
-  ]], {
-  	i(1, "app.css")
-  })),
+	s("html", fmt([[
+	<!DOCTYPE html>
+
+	<html>
+	<head>
+	<meta charset="utf-8">
+	<title>untitled</title>
+	</head>
+	<body>
+
+		{}
+
+	</body>
+	</html>
+	]], {
+		i(1)
+	})),
+
+	s("css", fmt([[
+		<link rel="stylesheet" type="text/css" href="{}">
+	]], {
+		i(1, "app.css")
+	})),
+
+	s("js", fmt([[
+		<script src="{}"></script>
+	]], {
+		i(1, "app.js")
+	})),
 
 }
