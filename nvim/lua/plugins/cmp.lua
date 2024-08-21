e338c6acaa7da9d133e7abb884157fcddfab516b
Oliver Vogel <oliver@olivervogel.com>
2024-08-21 15:28:51 +0200

    Fix missing variable

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -30,8 +30,8 @@ return {
                 ["<c-p>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif ls.choice_active() then
-                        ls.change_choice(-1)
+                    elseif require("luasnip").choice_active() then
+                        require("luasnip").change_choice(-1)
                     else
                         fallback()
                     end
@@ -39,8 +39,8 @@ return {
                 ["<c-n>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif ls.choice_active() then
-                        ls.change_choice(1)
+                    elseif require("luasnip").choice_active() then
+                        require("luasnip").change_choice(1)
                     else
                         fallback()
                     end
