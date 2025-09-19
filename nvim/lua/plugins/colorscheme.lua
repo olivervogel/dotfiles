d76a0e51848c7efe9ddbd25a72669fb13d04bcab
Oliver Vogel <oliver@olivervogel.com>
2025-09-19 13:23:42 +0200

    (nvim) Edit colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -80,7 +80,7 @@ return {
                TelescopeNormal = { bg = "#1f242c", fg = "palette.white" },
                TelescopeBorder = { bg = "#1f242c" },
 
-               Search = { bg = "palette.selection", fg = "palette.white.bright" },
+               Search = { bg = "palette.selection", fg = "palette.bg1" },
                IncSearch = { bg = "palette.white.bright", fg = "palette.bg1" },
                StatusLine = { bg = "palette.bg0", fg = "#3B4B5F" },
                WildMenu = { bg = "palette.bg0", fg = "#3B4B5F" },
