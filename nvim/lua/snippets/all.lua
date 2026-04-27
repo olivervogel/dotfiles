928bba2fdc9762fa16ec239d594e7f65dba08ba0
Oliver Vogel <o.vogel@wigital.de>
2026-04-27 08:15:23 +0200

    (nvim) Add command templates

--- nvim/lua/snippets/all.lua
+++ nvim/lua/snippets/all.lua
@@ -33,4 +33,21 @@ return {
 		t("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 	}),
 
+	-- -----------------------------------------------------------------------
+	-- command templates
+	-- -----------------------------------------------------------------------
+
+	s("cmd_curl", { 
+		t({"curl -X POST \\", ""}),
+		c(1, {
+			t({"-H \"Accept: text/markdown\" \\", ""}),
+			t(""),
+		}),
+		c(2, {
+			t({"-d '{\"key\": \"value\"}' \\", ""}),
+			t(""),
+		}),
+		t("https://"),
+		i(3, "example.com"),
+	}),
 }
