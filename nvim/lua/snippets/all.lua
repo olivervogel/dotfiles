66893205861d72c3af672f521faac14883a2ee92
Oliver Vogel <o.vogel@wigital.de>
2026-06-04 07:42:16 +0200

    (nvim) Add snippet

--- nvim/lua/snippets/all.lua
+++ nvim/lua/snippets/all.lua
@@ -33,6 +33,16 @@ return {
 		t("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 	}),
 
+
+	-- -----------------------------------------------------------------------
+	-- cron
+	-- -----------------------------------------------------------------------
+
+	s("cron", { 
+		t("* * * * * "),
+		t("cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1"),
+	}),
+
 	-- -----------------------------------------------------------------------
 	-- command templates
 	-- -----------------------------------------------------------------------
